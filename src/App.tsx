0f6a0abd487e3adddba8ec78b0a71c851be15a8c
sam nurollahi <samnurollahi>
2025-12-15 17:15:01 +0330

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -64,8 +64,8 @@ function App() {
           <button
             className={`cursor-pointer ${
               page == "word"
-                ? "bg-[#ba9f4f] text-white"
-                : "bg-[#dcdddf] text-[#ba9f4f]"
+                ? "bg-[#669999] text-white shadow-lg shadow-gray-400/60"
+                : "bg-[#dcdddf] text-[#669999] shadow-md shadow-gray-300/50"
             }  p-2 rounded-lg  transition-all duration-300 hover:rounded-4xl`}
             onClick={() => setPage("word")}
           >
@@ -74,8 +74,8 @@ function App() {
           <button
             className={`cursor-pointer ${
               page == "tablo"
-                ? "bg-[#ba9f4f] text-white"
-                : "bg-[#dcdddf] text-[#ba9f4f]"
+                ? "bg-[#669999] text-white shadow-lg shadow-gray-400/60"
+                : "bg-[#dcdddf] text-[#669999] shadow-md shadow-gray-300/50"
             } p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
             onClick={() => setPage("tablo")}
           >
@@ -90,41 +90,49 @@ function App() {
               key={"word"}
               className="animate__animated animate__fadeIn flex flex-wrap justify-between"
             >
-              <div className="w-[45%] mb-[25px]">
-                <label htmlFor="" className="text-[#303f67]">
+              <div className="w-[100%] mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
                   طول نوشته تابلو
                 </label>
-                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#f0edec] rounded-md border-t-2 border-t-[#ba9f4f]">
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
                   <span className="m-1">CM</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     value={tolTabloOne}
-                    onChange={(e) => setTolTabloOne(Number(e.target.value))}
+                    min="0"
+                    onChange={(e) => {
+                      const val = Number(e.target.value);
+                      setTolTabloOne(val < 0 ? 0 : val);
+                    }}
                   />
 
-                  <span>+</span>
+                  <span className="text-gray-500 text-[12px]">+</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     dir="ltr"
                     value={tolTabloTwo}
-                    onChange={(e) => setTolTabloTwo(Number(e.target.value))}
+                    min="0"
+                    onChange={(e) => {
+                      const val = Number(e.target.value);
+                      setTolTabloTwo(val < 0 ? 0 : val);
+                    }}
                   />
                 </div>
               </div>
 
-              <div className="w-[45%] mb-[25px]">
-                <label htmlFor="" className="text-[#303f67]">
+              <div className="w-full mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
                   نوع حروف
                 </label>
-                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#f0edec] rounded-md border-t-2 border-t-[#ba9f4f]">
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
                   <select
                     name=""
                     id=""
@@ -132,7 +140,7 @@ function App() {
                     onChange={(e) =>
                       setNoeHrof(e.target.value as "حروف سوعدی" | "حروف عادی")
                     }
-                    className="w-full border-0 outline-0"
+                    className="w-full border-0 outline-0 text-[#408080]"
                   >
                     <option value="حروف سوعدی">حروف سوعدی</option>
                     <option value="حروف عادی">حروف عادی</option>
@@ -140,43 +148,50 @@ function App() {
                 </div>
               </div>
 
-              <div className="w-[45%] mb-[25px]">
-                <label htmlFor="" className="text-[#303f67]">
+              <div className="w-full mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
                   ابعاد باکس
                 </label>
-                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#f0edec] rounded-md border-t-2 border-t-[#ba9f4f]">
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
                   <span className="m-1">CM</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     value={abadBoxOne}
-                    onChange={(e) => setAbadBoxOne(Number(e.target.value))}
+                    min="0"
+                    onChange={(e) => {
+                      const val = Number(e.target.value);
+                      setAbadBoxOne(val < 0 ? 0 : val);
+                    }}
                   />
 
-                  <span>X</span>
+                  <span className="text-gray-500 text-[12px]">X</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     dir="ltr"
-                    defaultValue={0}
                     value={abadBoxTwo}
-                    onChange={(e) => setAbadBoxTwo(Number(e.target.value))}
+                    min="0"
+                    onChange={(e) => {
+                      const val = Number(e.target.value);
+                      setAbadBoxTwo(val < 0 ? 0 : val);
+                    }}
                   />
                 </div>
               </div>
 
               <div className="w-full mb-[25px]">
-                <label htmlFor="" className="text-[#303f67]">
+                <label htmlFor="" className="text-[#c4ab74]">
                   آپشن ها
                 </label>
                 <div
-                  className="flex relative items-center w-full justify-between mt-3 px-3 py-3 bg-[#f0edec] rounded-md border-t-2 border-t-[#ba9f4f] cursor-pointer"
+                  className="flex relative items-center w-full justify-between mt-3 px-3 py-3 bg-[#ffffff] rounded-md border border-[#dde2d8] cursor-pointer"
                   onClick={() => setOptionBox(!optionBox)}
                 >
                   <div className="w-full">
@@ -201,7 +216,7 @@ function App() {
                   <div
                     className={`${
                       optionBox ? "" : "hidden"
-                    } absolute z-10 bottom-[-120px] w-full right-0 bg-[#f0edec] text-[#303f67] border border-t-0 border-[#ba9f4f] rounded-b-2xl overflow-hidden`}
+                    } absolute z-10 bottom-[-120px] w-full right-0 bg-[#f0edec] text-[#303f67] border border-t-0 border-[#ffffff] rounded-b-2xl overflow-hidden`}
                   >
                     <p
                       className="py-2 px-4 hover:bg-white w-full"
@@ -231,25 +246,26 @@ function App() {
               className="animate__animated animate__fadeIn flex flex-wrap justify-between"
             >
               <div className="w-[45%] mb-[25px]">
-                <label htmlFor="" className="text-[#303f67]">
+                <label htmlFor="" className="text-[#c4ab74]">
                   ابعاد نمای تابلو
                 </label>
-                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#f0edec] rounded-md border-t-2 border-t-[#ba9f4f]">
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
                   <span className="m-1">CM</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     defaultValue={0}
+                    min="0"
                   />
 
                   <span>X</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     dir="ltr"
@@ -258,25 +274,26 @@ function App() {
                 </div>
               </div>
               <div className="w-[45%] mb-[25px]">
-                <label htmlFor="" className="text-[#303f67]">
+                <label htmlFor="" className="text-[#c4ab74]">
                   ابعاد زیر تابلو
                 </label>
-                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#f0edec] rounded-md border-t-2 border-t-[#ba9f4f]">
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
                   <span className="m-1">CM</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     defaultValue={0}
+                    min="0"
                   />
 
                   <span>X</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     dir="ltr"
@@ -285,25 +302,26 @@ function App() {
                 </div>
               </div>
               <div className="w-[45%] mb-[25px]">
-                <label htmlFor="" className="text-[#303f67]">
+                <label htmlFor="" className="text-[#c4ab74]">
                   ابعاد بالای تابلو
                 </label>
-                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#f0edec] rounded-md border-t-2 border-t-[#ba9f4f]">
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
                   <span className="m-1">CM</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     defaultValue={0}
+                    min="0"
                   />
 
                   <span>X</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     dir="ltr"
@@ -312,25 +330,26 @@ function App() {
                 </div>
               </div>
               <div className="w-[45%] mb-[25px]">
-                <label htmlFor="" className="text-[#303f67]">
+                <label htmlFor="" className="text-[#c4ab74]">
                   ابعاد بغل تابلو
                 </label>
-                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#f0edec] rounded-md border-t-2 border-t-[#ba9f4f]">
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
                   <span className="m-1">CM</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     defaultValue={0}
+                    min="0"
                   />
 
                   <span>X</span>
 
                   <input
                     type="number"
-                    className=" w-[25%] outline-0"
+                    className="w-[25%] outline-0 text-[#408080]"
                     name=""
                     id=""
                     dir="ltr"
@@ -342,7 +361,17 @@ function App() {
           )}
         </div>
 
-        <div className="bg-white relative z-[-1] w-[95%] md:w-[500px] m-auto mt-[35px] p-4 rounded-3xl">
+        <div>
+          <button
+            id="mohasebe"
+            className="bg-[#669999] cursor-pointer block m-auto text-white  rounded-4xl px-18 py-2"
+          >
+            محاسبه
+          </button>
+        </div>
+
+        {/* tabel */}
+        <div className="bg-white relative z-[-1] w-[95%]  md:w-[500px] m-auto mt-[35px] p-4 rounded-3xl">
           {page == "word" ? (
             <table
               key={"word"}
@@ -377,6 +406,40 @@ function App() {
           ) : (
             <></>
           )}
+        </div>
+
+        {/* menu */}
+        <div className="bg-white fixed top-[50%] -translate-y-1/2 right-[25px]">
+          <nav className="flex flex-col gap-3">
+            <a
+              href=""
+              className={`cursor-pointer  bg-[#669999] text-white
+                p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
+            >
+              تماس با ما
+            </a>
+            <a
+              href=""
+              className={`cursor-pointer  bg-[#669999] text-white
+                p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
+            >
+              قوانین و مقررات
+            </a>
+            <a
+              href=""
+              className={`cursor-pointer  bg-[#669999] text-white
+                p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
+            >
+              حریم خصوصی
+            </a>
+            <a
+              href=""
+              className={`cursor-pointer  bg-[#669999] text-white
+               p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
+            >
+              درباره ما
+            </a>
+          </nav>
         </div>
 
         <br />
