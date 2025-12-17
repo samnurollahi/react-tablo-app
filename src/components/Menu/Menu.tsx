75de6df0f8e8ed4cf91feeb4c73d4dc74e7bafc2
sam nurollahi <samnurollahi>
2025-12-17 17:10:31 +0330

    train page

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+import styled from "./style.module.css";
+import { Link } from "react-router-dom";
+
+export default function () {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+  return (
+    <>
+      <div className=" fixed bottom-[15px] right-[25%] md:top-[20%] md:right-[25px] bg-transparent">
+        <nav className="flex flex-row md:flex-col gap-3">
+          <Link
+            to={"/"}
+            className={`cursor-pointer  bg-[#669999] text-white
+                p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
+          >
+            ماشین حساب
+          </Link>
+          <Link
+            to={"/train"}
+            className={`cursor-pointer  bg-[#669999] text-white
+               p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
+          >
+            اموزش
+          </Link>
+          <a
+            className={`cursor-pointer  bg-[#669999] text-white
+               p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
+            onClick={togglePopup}
+          >
+            درباره ما
+          </a>
+        </nav>
+      </div>
+
+      {isPopupOpen && (
+        <>
+          <div className="fixed inset-0  flex justify-center items-center z-50">
+            <div
+              className={`bg-white p-6 rounded-lg shadow-lg w-[90%] md:w-[400px] z-50 ${styled.animatedPopup}`}
+            >
+              <h2 className="text-xl font-bold mb-4">درباره ما</h2>
+              <p className="text-gray-700 mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias molestiae similique quam laudantium, iste consequatur
+                aut architecto quia impedit soluta accusamus alias labore,
+                consectetur iusto beatae consequuntur facere maiores quo!
+                09944576853
+              </p>
+              <button
+                className="bg-[#669999] cu text-white px-4 py-2 rounded-lg cursor-pointer"
+                onClick={togglePopup}
+              >
+                بستن
+              </button>
+            </div>
+            <div
+              className="fixed inset-0 bg-black opacity-80 z-40"
+              onClick={togglePopup}
+            ></div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
---
+++ src/page/Home/Home.tsx
@@ -0,0 +1,425 @@
+import { useEffect, useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+import Menu from "../../components/Menu/Menu";
+
+// interface Data {
+//   tolNeveshteTablo: number;
+//   noeHrof: "حروف سوعدی" | "حروف عادی";
+//   abadBox: number;
+//   opt: any[];
+// }
+
+function Home() {
+  const price = {
+    word: 700_000,
+    abad: 700_000,
+    option: {
+      "لبه پانچی": 100_000,
+      "لبه عادی": 200_000,
+      "آپشن رنگی": 500_000,
+    },
+  };
+
+  const [page, setPage] = useState<"word" | "tablo">("word");
+  const [optionBox, setOptionBox] = useState<boolean>(false);
+  const [optionList, setOptionList] = useState<string[]>([]);
+  const [tolTabloOne, setTolTabloOne] = useState<number>(0);
+  const [tolTabloTwo, setTolTabloTwo] = useState<number>(0);
+  const [priceText, setPriceText] = useState<number>(0);
+  const [noeHrof, setNoeHrof] = useState<"حروف سوعدی" | "حروف عادی">(
+    "حروف سوعدی"
+  );
+  const [abadBoxOne, setAbadBoxOne] = useState<number>(0);
+  const [abadBoxTwo, setAbadBoxTwo] = useState<number>(0);
+  const [priceBox, setPriceBox] = useState<number>(0);
+  const [otpPrice, setOtpPrice] = useState<number>(0);
+
+  useEffect(() => {
+    setPriceText((((tolTabloOne + tolTabloTwo) * 5) / 100) * price.word);
+  }, [tolTabloOne, tolTabloTwo]);
+  useEffect(() => {
+    setPriceBox((abadBoxOne / 100 + abadBoxTwo / 100) * 2.5 * price.abad);
+  }, [abadBoxOne, abadBoxTwo]);
+  useEffect(() => {
+    optionList.forEach((otp) => {
+      otp = otp.trim();
+      setOtpPrice(
+        (prev) => +prev + price.option[otp as keyof typeof price.option]
+      );
+    });
+  }, [optionList]);
+
+  const handleOptionClick = (option: string) => {
+    if (optionList.includes(option)) {
+      setOptionList(optionList.filter((item) => item !== option));
+    } else {
+      setOptionList([...optionList, option]);
+    }
+  };
+
+  return (
+    <>
+      <div className="vazir">
+        {/* header */}
+        <div className="bg-white flex justify-around py-[35px]  w-[95%] md:w-[500px] m-auto rounded-b-3xl">
+          <button
+            className={`cursor-pointer ${
+              page == "word"
+                ? "bg-[#669999] text-white shadow-lg shadow-gray-400/60"
+                : "bg-[#dcdddf] text-[#669999] shadow-md shadow-gray-300/50"
+            }  p-2 rounded-lg  transition-all duration-300 hover:rounded-4xl`}
+            onClick={() => setPage("word")}
+          >
+            محاسبه حروف
+          </button>
+          <button
+            className={`cursor-pointer ${
+              page == "tablo"
+                ? "bg-[#669999] text-white shadow-lg shadow-gray-400/60"
+                : "bg-[#dcdddf] text-[#669999] shadow-md shadow-gray-300/50"
+            } p-2 rounded-lg transition-all duration-300 hover:rounded-4xl`}
+            onClick={() => setPage("tablo")}
+          >
+            محاسبه تابلو
+          </button>
+        </div>
+
+        {/* form */}
+        <div className="bg-white w-[95%] md:w-[500px] m-auto mt-[35px] p-4 rounded-3xl transition-all">
+          {page == "word" ? (
+            <div
+              key={"word"}
+              className="animate__animated animate__fadeIn flex flex-wrap justify-between"
+            >
+              <div className="w-full mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
+                  طول نوشته تابلو
+                </label>
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
+                  <span className="m-1">CM</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    value={tolTabloOne}
+                    min="0"
+                    onChange={(e) => {
+                      const val = Number(e.target.value);
+                      setTolTabloOne(val < 0 ? 0 : val);
+                    }}
+                  />
+
+                  <span className="text-gray-500 text-[12px]">+</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    dir="ltr"
+                    value={tolTabloTwo}
+                    min="0"
+                    onChange={(e) => {
+                      const val = Number(e.target.value);
+                      setTolTabloTwo(val < 0 ? 0 : val);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="w-full mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
+                  نوع حروف
+                </label>
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
+                  <select
+                    name=""
+                    id=""
+                    value={noeHrof}
+                    onChange={(e) =>
+                      setNoeHrof(e.target.value as "حروف سوعدی" | "حروف عادی")
+                    }
+                    className="w-full border-0 outline-0 text-[#408080]"
+                  >
+                    <option value="حروف سوعدی">حروف سوعدی</option>
+                    <option value="حروف عادی">حروف عادی</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="w-full mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
+                  ابعاد باکس
+                </label>
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
+                  <span className="m-1">CM</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    value={abadBoxOne}
+                    min="0"
+                    onChange={(e) => {
+                      const val = Number(e.target.value);
+                      setAbadBoxOne(val < 0 ? 0 : val);
+                    }}
+                  />
+
+                  <span className="text-gray-500 text-[12px]">X</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    dir="ltr"
+                    value={abadBoxTwo}
+                    min="0"
+                    onChange={(e) => {
+                      const val = Number(e.target.value);
+                      setAbadBoxTwo(val < 0 ? 0 : val);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="w-full mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
+                  آپشن ها
+                </label>
+                <div
+                  className="flex relative items-center w-full justify-between mt-3 px-3 py-3 bg-[#ffffff] rounded-md border border-[#dde2d8] cursor-pointer"
+                  onClick={() => setOptionBox(!optionBox)}
+                >
+                  <div className="w-full">
+                    {optionList.length > 0 ? (
+                      optionList.map((option, index) => (
+                        <span
+                          key={index}
+                          className="bg-white p-1 rounded-full inline-flex items-center text-sm mr-1"
+                        >
+                          <AiOutlineCloseCircle
+                            className="text-[#ba9f4f] cursor-pointer ml-1"
+                            onClick={() => handleOptionClick(option)}
+                          />
+                          {option}
+                        </span>
+                      ))
+                    ) : (
+                      <span className="text-[#303f67]">انتخاب کنید</span>
+                    )}
+                  </div>
+
+                  <div
+                    className={`${
+                      optionBox ? "" : "hidden"
+                    } absolute z-10 bottom-[-120px] w-full right-0 bg-[#f0edec] text-[#303f67] border border-t-0 border-[#ffffff] rounded-b-2xl overflow-hidden`}
+                  >
+                    <p
+                      className="py-2 px-4 hover:bg-white w-full"
+                      onClick={() => handleOptionClick("لبه پانچی")}
+                    >
+                      لبه پانجی
+                    </p>
+                    <p
+                      className="py-2 px-4 hover:bg-white w-full"
+                      onClick={() => handleOptionClick("لبه عادی")}
+                    >
+                      لبه عادی
+                    </p>
+                    <p
+                      className="py-2 px-4 hover:bg-white w-full"
+                      onClick={() => handleOptionClick("آپشن رنگی")}
+                    >
+                      آپشن رنگی
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div
+              key={"tablo"}
+              className="animate__animated animate__fadeIn flex flex-wrap justify-between"
+            >
+              <div className="w-[45%] mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
+                  ابعاد نمای تابلو
+                </label>
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
+                  <span className="m-1">CM</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    defaultValue={0}
+                    min="0"
+                  />
+
+                  <span>X</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    dir="ltr"
+                    defaultValue={0}
+                  />
+                </div>
+              </div>
+              <div className="w-[45%] mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
+                  ابعاد زیر تابلو
+                </label>
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
+                  <span className="m-1">CM</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    defaultValue={0}
+                    min="0"
+                  />
+
+                  <span>X</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    dir="ltr"
+                    defaultValue={0}
+                  />
+                </div>
+              </div>
+              <div className="w-[45%] mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
+                  ابعاد بالای تابلو
+                </label>
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
+                  <span className="m-1">CM</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    defaultValue={0}
+                    min="0"
+                  />
+
+                  <span>X</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    dir="ltr"
+                    defaultValue={0}
+                  />
+                </div>
+              </div>
+              <div className="w-[45%] mb-[25px]">
+                <label htmlFor="" className="text-[#c4ab74]">
+                  ابعاد بغل تابلو
+                </label>
+                <div className="flex items-center w-full justify-between mt-3 px-3 py-2 bg-[#ffffff] rounded-md border border-[#dde2d8]">
+                  <span className="m-1">CM</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    defaultValue={0}
+                    min="0"
+                  />
+
+                  <span>X</span>
+
+                  <input
+                    type="number"
+                    className="w-[25%] outline-0 text-[#408080]"
+                    name=""
+                    id=""
+                    dir="ltr"
+                    defaultValue={0}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <button
+            id="mohasebe"
+            className="bg-[#669999] cursor-pointer block m-auto text-white  rounded-4xl px-18 py-2 relative z-[-1]"
+          >
+            محاسبه
+          </button>
+        </div>
+
+        {/* tabel */}
+        <div className="bg-white relative z-[-1] w-[95%]  md:w-[500px] m-auto mt-[35px] p-4 rounded-3xl">
+          {page == "word" ? (
+            <table
+              key={"word"}
+              className="animate__animated animate__fadeIn  table-auto w-full text-right border-collapse"
+            >
+              <thead className="bg-[#f0edec]">
+                <tr>
+                  <th className="p-3 text-[#303f67]">نوع سفارش</th>
+                  <th className="p-3 text-[#303f67]">تعداد</th>
+                  <th className="p-3 text-[#303f67]">مبلغ نهایی</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td className="p-3 ">{noeHrof}</td>
+                  <td className="p-3 ">2 سطر</td>
+                  <td className="p-3 ">{priceText.toLocaleString()}</td>
+                </tr>
+                <tr>
+                  <td className="p-3 ">آپشن ها</td>
+                  <td className="p-3 ">{optionList.length} عدد</td>
+                  <td className="p-3 ">{otpPrice.toLocaleString()}</td>
+                </tr>
+                <tr>
+                  <td className="p-3 ">باکس</td>
+                  <td className="p-3 ">1 عدد</td>
+                  <td className="p-3 ">{priceBox.toLocaleString()}</td>
+                </tr>
+              </tbody>
+            </table>
+          ) : (
+            <></>
+          )}
+        </div>
+
+        {/* menu */}
+        <Menu />
+
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
+}
+
+export default Home;
